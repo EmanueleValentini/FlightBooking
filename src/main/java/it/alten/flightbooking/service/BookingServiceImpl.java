9eddb10b8ba943b0aa530bcb7166120c3d1034dd
Emanuele <emanuele_valentini@hotmail.it>
2023-03-10 10:58:30 +0100

    Create BookingServiceImpl.java

---
+++ src/main/java/it/alten/flightbooking/service/BookingServiceImpl.java
@@ -0,0 +1,36 @@
+package it.alten.flightbooking.service;
+
+import it.alten.flightbooking.model.Booking;
+import it.alten.flightbooking.repository.BookingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class BookingServiceImpl implements BookingService{
+
+    @Autowired
+    BookingRepository repo;
+
+    @Override
+    public boolean bookFlight(String flightNumber, Date flightDate, String passengerName, int numSeats) {
+        if(repo.findBookingsByFlightNumberAndFlightDate(flightNumber,flightDate).isEmpty() );
+    }
+
+    @Override
+    public List<Booking> getFlightBookings(String flightNumber, Date flightDate) {
+        return null;
+    }
+
+    @Override
+    public List<Booking> getPassengerBooklings(String passengerName) {
+        return null;
+    }
+
+    @Override
+    public boolean cancelBooking(Long id) {
+        return false;
+    }
+}
