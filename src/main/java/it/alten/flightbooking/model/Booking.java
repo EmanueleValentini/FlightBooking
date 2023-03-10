824a07bcc4e2e1dba82a6d17010aa5aae9ffebed
Emanuele <emanuele_valentini@hotmail.it>
2023-03-10 10:09:28 +0100

    Booking & BookingDto

---
+++ src/main/java/it/alten/flightbooking/model/Booking.java
@@ -0,0 +1,40 @@
+package it.alten.flightbooking.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Date;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table (name="booking")
+public class Booking implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date flightDate;
+
+    @DateTimeFormat(pattern = "HH:mm:ss")
+    private LocalDateTime depatureTime;
+
+    @DateTimeFormat(pattern = "HH:mm:ss")
+    private LocalDateTime arrivalTime;
+
+    private String landingAirport;
+
+    private String takeOffAirport;
+
+    private Integer seatsBooked;
+
+    private String passengerName;
+
+}
---
+++ src/main/java/it/alten/flightbooking/model/dto/BookingDto.java
@@ -0,0 +1,32 @@
+package it.alten.flightbooking.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BookingDto {
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date flightDate;
+
+    @DateTimeFormat(pattern = "HH:mm:ss")
+    private LocalDateTime depatureTime;
+
+    @DateTimeFormat(pattern = "HH:mm:ss")
+    private LocalDateTime arrivalTime;
+
+    private String landingAirport;
+
+    private String takeOffAirport;
+
+    private Integer seatsBooked;
+
+    private String passengerName;
+}
